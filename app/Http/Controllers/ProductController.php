8c53c3dff4b3ee450b3ed2eb7397d7f8eb3c3731
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2023-01-02 15:58:18 +0800

    changed to using db facade

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,11 +8,12 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 
 {
-    public function all_products() {
+    public function all_products()
+    {
         $data = Product::all();
         return view('dashboard', ['products' => $data]);
     }
-    
+
     public function add_product(Request $request)
     {
         // Validate
@@ -23,53 +24,35 @@ class ProductController extends Controller
             'sale_price' => ['required'],
         ]);
         $product = new Product;
+        $product->add($fields['product_name'], $fields['quantity'], $fields['cost_price'], $fields['sale_price']);
 
-        $product->product_name = $fields['product_name'];
-        $product->quantity = $fields['quantity'];
-        $product->cost_price = $fields['cost_price'];
-        $product->sale_price = $fields['sale_price'];
-
-        // Insert data to the database
-        $product->save();
-        
         // Redirects user to the previous page
         return redirect('dashboard')->with('message', 'Successfuly added a product!');
     }
 
-    public function retrieve_product($id) {
-        $product = Product::findOrFail($id);
-        return view('product.edit', ['product' => $product]);
-    }
-
     //
-    public function update_product(Request $request) {
+    public function update_product(Request $request)
+    {
         $fields = $request->validate([
-            'id' => ['required'],
+            'product_id' => ['required'],
             'product_name' => ['required'],
             'quantity' => ['required'],
             'cost_price' => ['required'],
             'sale_price' => ['required'],
         ]);
-        $id = $fields['id'];
-        $product = Product::findOrFail($id);
-
-        $product->product_name = $fields['product_name'];
-        $product->quantity = $fields['quantity'];
-        $product->cost_price = $fields['cost_price'];
-        $product->sale_price = $fields['sale_price'];
-
-        // Insert data to the database
-        $product->save();
-        
+        $product_id = $fields['product_id'];
+        $product = new Product();
+        $product->edit($product_id, $fields['product_name'], $fields['quantity'], $fields['cost_price'], $fields['sale_price']);
         // Redirects user to the previous page
         return redirect('dashboard')->with('message', 'Successfuly edited a product!');
     }
 
+    public function delete_product(Request $request)
+    {
+        $product_id = $request->product_id;
 
-    public function delete_product(Request $request) {
-        $id = $request->id;
-        $product = Product::findOrFail($id);
-        $product->delete();
+        $product = new Product();
+        $product->delete_product($product_id);
         return back()->with('message', 'Successfuly deleted a product!');
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,12 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Product extends Model
 {
     use HasFactory;
     public $timestamps = false;
+    public $primaryKey = 'product_id';
 
     // Declares what table is the model
-    public $table = 'product'; 
+    public $table = 'product';
+
+    // the add method allows you to insert a new product in the table "product"
+    public function add($product_name, $quantity, $cost_price, $sale_price)
+    {
+        // Retrieves the name of the inventory
+        $inventory_name = Inventory::show()->inventory_name;
+        DB::insert('INSERT INTO product (`product_name`, `quantity`, `cost_price`, `sale_price`, `inventory_name`) VALUES (?, ?, ?, ?, ?)', [$product_name, $quantity, $cost_price, $sale_price, $inventory_name]);
+    }
+    //  the edit method allows you to edit a product
+    public function edit($product_id, $product_name, $quantity, $cost_price, $sale_price)
+    {
+        DB::update('UPDATE product SET `product_name` = ?,`quantity` = ?, `cost_price` = ?, `sale_price` = ? WHERE `product_id` = ?;', [$product_name, $quantity, $cost_price, $sale_price, $product_id]);
+    }
+    //  the delete method allows you to delete a product
+    public function delete_product($product_id) {
+        DB::delete('DELETE FROM product WHERE product_id = ?', [$product_id]);
+    }
 }
