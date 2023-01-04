b9a8f083a43cae03a7cdce1b0a2bb45c7825bb4b
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2023-01-04 10:00:03 +0800

    Create edit_receipt.js

---
+++ public/js/edit_receipt.js
@@ -0,0 +1,9 @@
+function editBtnClicked() {
+    document.getElementById("customer_name").disabled = false;
+    document.getElementById("save_button").disabled = false;
+    document.getElementById("address").disabled = false;
+    document.getElementById("phone_number").disabled = false;
+    document.getElementById("customer_name").style.color = "black";
+    document.getElementById("address").style.color = "black";
+    document.getElementById("phone_number").style.color = "black";
+}
