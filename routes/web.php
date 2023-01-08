5040a2ade13163e3cc0cf8c1cd529a8eae1c6dbe
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2023-01-08 14:50:37 +0800

    grouped the login routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\CustomerReceiptController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProductController;
-use App\Models\CustomerReceipt;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,31 +16,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/', [EmployeeController::class, 'login'])->name('auth');
 
-Route::get('/', function () {
-    return view('login');
-})->name('login');
+// Login routes
+Route::prefix('/')->group(function () {
+    Route::post('/', [EmployeeController::class, 'login'])->name('auth');
 
+    Route::get('/', function () {
+        return view('login');
+    })->name('login');
+});
 
 // Protected routes
 Route::middleware(['auth'])->group(function () {
     Route::prefix('product')->group(function () {
-        // /product
+
+        // /Product routes
         Route::get('/', [ProductController::class, 'render_data'])->name('product.page');
         Route::post('/', [ProductController::class, 'add_product'])->name('add.product');
         Route::put('/', [ProductController::class, 'update_product'])->name('edit.product');
     });
 
     Route::prefix('receipt')->group(function () {
-        Route::get('/add-test', function () {
-            return view('receipt.add');
-        });
+
+        // Receipt routes
         Route::get('/', [CustomerReceiptController::class, 'render_data'])->name('receipt.page');
         Route::get('/{receipt_id}', [CustomerReceiptController::class, 'retrieve_customer_receipt'])->name('edit.receipt.page');
         Route::post('/', [CustomerReceiptController::class, 'create_customer_receipt'])->name('add.receipt');
         Route::put('/', [CustomerReceiptController::class, 'edit_customer_information_in_receipt'])->name('edit.receipt');
-        Route::delete('/',[CustomerReceiptController::class, 'delete_customer_receipt'])->name('delete.receipt');
+        Route::delete('/', [CustomerReceiptController::class, 'delete_customer_receipt'])->name('delete.receipt');
     });
 
     // Logout
