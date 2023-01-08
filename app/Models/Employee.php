e5cc649a2b28e6d76c269d78dd3a4911576c97a9
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2023-01-08 20:51:35 +0800

    removed laravel sanctum

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,11 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class Employee extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use  HasFactory, Notifiable;
     public $timestamps = false; // removes the updated_at column 
     /**
      * The attributes that are mass assignable.
