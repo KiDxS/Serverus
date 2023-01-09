4053a8f5efc593e9ba588d31c7f4d1448ca3b5c9
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2023-01-09 08:37:16 +0800

    removed unnecessary seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,53 +25,6 @@ class DatabaseSeeder extends Seeder
         \App\Models\Employee::factory(2)->create();
         \App\Models\Product::factory(4)->create();
         \App\Models\Customer::factory(4)->create();
-        // $this->customerRecordSeed();
-        // $this->customerCartSeed();
-        // $this->customerReceiptSeed();
-    }
 
-    private function firstProduct()
-    {
-        $products = \App\Models\Product::all();
-        $product = $products[0];
-        return $product;
-    }
-    private function customerReceiptSeed()
-    {
-        \App\Models\CustomerReceipt::factory()->create();
-
-        // Retrieves the sale price of the product and the quantity to calculate its total
-        $result = DB::select('SELECT (product.sale_price * customer_cart.quantity) AS total FROM customer_receipt, customer_cart, product
-        where (customer_receipt.cart_id = customer_cart.cart_id AND customer_receipt.cart_id = 1) 
-        AND 
-        (customer_cart.product_id = product.product_id AND customer_cart.product_id = 1);');
-
-        // Updates the value of the total of the receipt
-        DB::update('UPDATE `customer_receipt` SET `total` = ? WHERE `receipt_id` = 1; ', [$result[0]->total]);
-    }
-    private function customerCartSeed()
-    {
-        $customers = \App\Models\Customer::all();
-
-        $customer = $customers[0];
-        $product = $this->firstProduct();
-        $customer_name = $customer->customer_name;
-        $product_id = $product->product_id;
-        \App\Models\CustomerCart::factory()->create([
-            'product_id' => $product_id,
-            'customer_name' => $customer_name
-        ]);
-    }
-
-
-    private function customerRecordSeed()
-    {
-        $customers = \App\Models\Customer::all();
-        foreach ($customers as $customer) {
-            $customer_name = $customer->customer_name;
-            \App\Models\CustomerRecord::factory()->create([
-                'customer_name' => $customer_name
-            ]);
-        }
     }
 }
